6c8e25ccd2de6c815e995b0fa61ed8322cee7daf
Jameel Ali <Jameelali0101@gmail.com>
2026-02-03 16:26:43 +0000

    add the delete, clear logic

---
+++ script.js
@@ -0,0 +1,42 @@
+const numbers = document.querySelectorAll("#numbers button")
+const screen = document.getElementById("screen")
+const dot = document.querySelector(".dot")
+const clear = document.querySelector(".clear")
+const zero = document.querySelector(".zero")
+const deleted = document.querySelector(".delete")
+const lbracket = document.querySelector(".leftbracket")
+const rbracket = document.querySelector(".rightbracket")
+
+
+numbers.forEach(number => number.addEventListener("click", function() {
+    screen.textContent += this.textContent;
+}))
+
+dot.addEventListener("click", function() {
+    screen.textContent += this.textContent;   
+},{once:true})
+
+zero.addEventListener("click", function() {
+    screen.textContent += this.textContent;   
+})
+
+lbracket.addEventListener("click", function() {
+    screen.textContent += this.textContent;   
+})
+
+rbracket.addEventListener("click", function() {
+    screen.textContent += this.textContent;   
+})
+
+clear.addEventListener("click", () => {
+    screen.textContent = ''
+})
+
+deleted.addEventListener("click", () => {
+    const query = screen.textContent;
+    screen.textContent = query.slice(0,query.length-1)
+})
+
+
+
+
