aac9cba4efb43e1812af3abd0487bc0eb72c75af
Jameel Ali <Jameelali0101@gmail.com>
2026-02-07 19:34:59 +0000

    comment the dozens of unnecessary selectors(we can use onClick in html for each button

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-const numbers = document.querySelectorAll("#numbers button")
-const screen = document.getElementById("screen")
-const dot = document.querySelector(".dot")
+// const numbers = document.querySelectorAll("#numbers button")
+// const screen = document.getElementById("screen")
+// const dot = document.querySelector(".dot")
 const clear = document.querySelector(".clear")
-const zero = document.querySelector(".zero")
+// const zero = document.querySelector(".zero")
 const deleted = document.querySelector(".delete")
-const lbracket = document.querySelector(".leftbracket")
-const rbracket = document.querySelector(".rightbracket")
-const operators = document.querySelectorAll(".op")
+// const lbracket = document.querySelector(".leftbracket")
+// const rbracket = document.querySelector(".rightbracket")
+// const operators = document.querySelectorAll(".op")
 const result = document.querySelector(".result")
 
-const addition = document.querySelector(".add")
-const subtraction = document.querySelector(".subtract")
-const multiplication = document.querySelector(".multiply")
-const division = document.querySelector(".divide")
-const exponention = document.querySelector(".exponent")
-const remainder = document.querySelector(".remainder")
-const sqrt = document.querySelector(".sqrt")
+// const addition = document.querySelector(".add")
+// const subtraction = document.querySelector(".subtract")
+// const multiplication = document.querySelector(".multiply")
+// const division = document.querySelector(".divide")
+// const exponention = document.querySelector(".exponent")
+// const remainder = document.querySelector(".remainder")
+// const sqrt = document.querySelector(".sqrt")
 
-numbers.forEach(number => number.addEventListener("click", function() {
-    screen.textContent += this.textContent;
-}))
 
-dot.addEventListener("click", function() {
-    screen.textContent += this.textContent;   
-})
+// numbers.forEach(number => number.addEventListener("click", function() {
+//     screen.textContent += this.textContent;
+// }))
 
-zero.addEventListener("click", function() {
-    screen.textContent += this.textContent;   
-})
+// dot.addEventListener("click", function() {
+//     screen.textContent += this.textContent;   
+// })
 
-lbracket.addEventListener("click", function() {
-    screen.textContent += this.textContent;   
-})
+// zero.addEventListener("click", function() {
+//     screen.textContent += this.textContent;   
+// })
 
-rbracket.addEventListener("click", function() {
-    screen.textContent += this.textContent;   
-})
+// lbracket.addEventListener("click", function() {
+//     screen.textContent += this.textContent;   
+// })
+
+// rbracket.addEventListener("click", function() {
+//     screen.textContent += this.textContent;   
+// })
 
 clear.addEventListener("click", () => {
     screen.textContent = ''
@@ -46,27 +47,27 @@ deleted.addEventListener("click", () => {
     screen.textContent = query.slice(0,query.length-1)
 })
 
-operators.forEach(operator => operator.addEventListener("click", function() {
-    screen.textContent += this.textContent;
-}))
+// operators.forEach(operator => operator.addEventListener("click", function() {
+//     screen.textContent += this.textContent;
+// }))
 
-result.addEventListener("click", () => {
-    const numbers = [];
-    const operators = [];
-    for(let i=0; i<screen.textContent.length; i++) {
-        if(screen.textContent[i] === '+' ||
-            screen.textContent[i] === '-' ||
-            screen.textContent[i] === 'x' ||
-            screen.textContent[i] === '/' ||
-            screen.textContent[i] === '%' ||
-            screen.textContent[i] === '^' ||
-            screen.textContent[i] === '√') 
-            {
-            operators.push(screen.textContent[i])
-        }
-        else {
-            numbers.push(Number(screen.textContent[i]))
-        }
-    }
-    console.log(numbers, operators)
-})
+// result.addEventListener("click", () => {
+//     const operators = [];
+//     const numbers = screen.textContent.match(/\d+/g,'')
+//     for(let i=0; i<screen.textContent.length; i++) {
+//         if(screen.textContent[i] === '+' ||
+//             screen.textContent[i] === '-' ||
+//             screen.textContent[i] === 'x' ||
+//             screen.textContent[i] === '/' ||
+//             screen.textContent[i] === '%' ||
+//             screen.textContent[i] === '^' ||
+//             screen.textContent[i] === '√' ||
+//             screen.textContent[i] === '(' ||
+//             screen.textContent[i] === ')')
+//             {
+//             operators.push(screen.textContent[i])
+//         }
+//     }
+ 
+// })
+
